5bfa6b5c03e6a4ac89faf4a93385712eb93ef6b1
Kevin Deyne <kevindeyne@gmail.com>
2021-02-06 12:11:11 -0800

    Added used dependencies, removed startup code

--- src/main/java/com/example/deepdivejavaversions/DeepdiveJavaVersionsApplication.java
+++ src/main/java/com/example/deepdivejavaversions/DeepdiveJavaVersionsApplication.java
@@ -1,13 +1,9 @@
 package com.example.deepdivejavaversions;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DeepdiveJavaVersionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeepdiveJavaVersionsApplication.class, args);
+
 	}
 
 }
